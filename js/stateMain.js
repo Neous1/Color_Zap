6d64c7b878c46c127124eba2678c689939330e72
Neous1 <Neous1@users.noreply.github.com>
2017-04-27 00:36:16 -0400

    initial commit

---
+++ js/stateMain.js
@@ -0,0 +1,21 @@
+var StateMain={    
+    
+   preload:function()
+    {
+       // load images into library
+    },
+    
+    create:function()
+    {
+        console.log("ready")
+        //set up objects, variables 
+        // sounds, text
+        // good guys, explosions
+    },
+    
+    update:function()
+    {       
+        // constant running loop
+    }    
+    
+}
